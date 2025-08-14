634f4105fac0bb733060d8b24684293b564f3c3f
MINSsss96 <minsue0422@naver.com>
2025-08-14 15:44:55 +0900

    1. 삭제처리 완료 2. PathVariable을 사용해서 처리함.

--- src/main/java/com/my/member/service/MemberService.java
+++ src/main/java/com/my/member/service/MemberService.java
@@ -47,15 +47,23 @@ public class MemberService {
 
     public void insertMember(MemberDto dto) {
         // 3. 서비스에서 DTO를 entity로 바꾼다.
-        Member member = new Member();
-        member.setName(dto.getName());
-        member.setAge(dto.getAge());
-        member.setAddress(dto.getAddress());
+//        Member member = new Member();
+//        member.setName(dto.getName());
+//        member.setAge(dto.getAge());
+//        member.setAddress(dto.getAddress());
+        // 우리가 만든 toDto를 이용해서 member 엔티티 생성핫기
+        Member member = MemberDto.toDto(dto);
 
         // 4. repository를 이용해서 저장한다.
         member = repository.save(member);
         System.out.println("===================");
         System.out.println(member);
+
+    }
+
+    public void deleteMember(Long id) {
+        // 삭제처리
+        repository.deleteById(id);
 
     }
 }
